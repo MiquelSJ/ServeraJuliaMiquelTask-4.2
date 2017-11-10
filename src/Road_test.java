6ef41964ae3ba45ee6b8dab021e7825535778154
MiquelSJ <32461614+MiquelSJ@users.noreply.github.com>
2017-11-10 20:42:41 +0100

    Update Road_test.java

--- src/Road_test.java
+++ src/Road_test.java
@@ -2,48 +2,50 @@ import transport.Road;
 import transport.Vehicle;
 
 public class Road_test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //Nom de la carretera
+        Road road = new Road("de Porreres a Muntuïri");
 
-       Road carretera = new Road("Vella de Sineu");
+        //Els vehicles que es fiquen a la carretera
+        road.addVehicle("Toyota",100);
+        road.addVehicle("Honda",200);
+        road.addVehicle("Renault",300);
+        road.addVehicle("Audi",400);
+        road.addVehicle("BMW", 500);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
 
-        carretera.addVehicle(100);
-        carretera.addVehicle(200);
-        carretera.addVehicle(300);
-        carretera.addVehicle(400);
-        carretera.addVehicle(500);
-        carretera.addVehicle(600);
+        //Mostra el nombre de cotxes que s'han ficat a la carretera
+        System.out.println("A la carretera s'hi han ficat "+road.getNumberOfVehicles()+" vehicles");
 
-        Vehicle veh1 = carretera.getVehicle(0);
-        veh1.addBox(100);
-        veh1.addBox(200);
-        System.out.println("El vehicle amb ID "+ veh1+ " té una carrega de " +veh1.getLoad());
-        System.out.println("------------------------------------");
 
-        Vehicle veh2 = carretera.getVehicle(1);
-        veh2.addBox(100);
-        veh2.addBox(100);
-        System.out.println("El vehicle amb ID "+ veh2+ " té una carrega de " +veh1.getLoad());
-        System.out.println("------------------------------------");
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
 
-        Vehicle veh3 = carretera.getVehicle(2);
-        veh3.addBox(100);
-        veh3.addBox(200);
-        System.out.println("El vehicle amb ID "+ veh3+ " té una carrega de " +veh1.getLoad());
-        System.out.println("------------------------------------");
+        //Característiques dels cotxes que s'han ficat a la carretera
+        Vehicle vehicle1 = road.getVehicle(0);
+        road.getVehicle(0).addBox(100);
+        vehicle1.addBox(150);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
 
-        Vehicle veh4 = carretera.getVehicle(3);
-        veh4.addBox(100);
-        veh4.addBox(200);
-        veh4.addBox(150);
-        System.out.println("El vehicle amb ID "+ veh4+ " té una carrega de " +veh1.getLoad());
-        System.out.println("------------------------------------");
+        Vehicle vehicle2 = road.getVehicle(1);
+        vehicle2.addBox(100);
+        vehicle2.addBox(100);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
 
-        Vehicle veh5 = carretera.getVehicle(4);
-        veh5.addBox(100);
-        veh5.addBox(200);
-        veh5.addBox(150);
-        System.out.println("El vehicle amb ID "+ veh5+ " té una carrega de " +veh1.getLoad());
-        System.out.println("------------------------------------");
+        Vehicle vehicle3 = road.getVehicle(2);
+        vehicle3.addBox(100);
+        vehicle3.addBox(200);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
+
+        Vehicle vehicle4 = road.getVehicle(3);
+        vehicle4.addBox(100);
+        vehicle4.addBox(150);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
+
+        Vehicle vehicle5 = road.getVehicle(4);
+        vehicle5.addBox(100);
+        vehicle5.addBox(200);
+        System.out.println("ºººººººººººººººººººººººººººººººººººº");
+
     }
 
 
